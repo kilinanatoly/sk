d9e2f8cb24691bf880fa4792ece28db753437ef0
kilinanatoly <kilinanatoly@gmail.com>
2020-12-10 10:28:28 +0300

    'fix'

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     });
     $(document).mouseup(function (e){ // событие клика по веб-документу
         var div = $(".header1__inner-bar-menu"); // тут указываем ID элемента
-        var di2 = $(".header-burger"); // тут указываем ID элемента
+        var div2 = $(".header-burger"); // тут указываем ID элемента
         if ((!div.is(e.target) && div.has(e.target).length === 0) && (!div2.is(e.target) && div2.has(e.target).length === 0)) {
             if ($('body').hasClass('state-menu')){
                 $('body').removeClass('state-menu');
